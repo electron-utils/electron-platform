d6759325f93d4a79e97ec62a6a162b28a905935b
Johnny Wu <KarasAya@gmail.com>
2016-12-28 09:40:28 +0800

    update tests

--- test/electron.spec.js
+++ test/electron.spec.js
@@ -4,13 +4,38 @@ const path = require('path');
 const electron = require('electron');
 const {Application} = require('spectron');
 const assert = require('assert');
-const {expect} = require('chai');
 
-describe("electron", function () {
+// DISABLE: tap failed with spectron
+// issue reported: https://github.com/electron/spectron/issues/155
+// const tap = require('tap');
+// tap.test('electron', {autoend: false, timeout: 0}, t => {
+//   let app = null;
+
+//   t.beforeEach(() => {
+//     app = new Application({
+//       path: electron,
+//       args: [path.join(__dirname, 'fixtures', 'app')]
+//     });
+//     return app.start();
+//   });
+
+//   t.afterEach(() => {
+//     if (app && app.isRunning()) {
+//       return app.stop();
+//     }
+//   });
+
+//   t.test('should be ok in main process', () => {
+//     t.equal('foo', 'foo');
+//     t.end();
+//   });
+// });
+
+describe('electron', function () {
   this.timeout(0);
   let app = null;
 
-  beforeEach(function () {
+  before(function () {
     app = new Application({
       path: electron,
       args: [path.join(__dirname, 'fixtures', 'app')]
@@ -18,7 +43,7 @@ describe("electron", function () {
     return app.start();
   });
 
-  afterEach(function () {
+  after(function () {
     if (app && app.isRunning()) {
       return app.stop();
     }
@@ -43,24 +68,24 @@ describe("electron", function () {
       .waitUntilWindowLoaded()
       .getRenderProcessLogs()
       .then(function (logs) {
-        expect(logs.length).to.equal(8);
-        expect(logs[0].message).to.contains('isNode: true');
-        expect(logs[1].message).to.contains('isElectron: true');
-        expect(logs[2].message).to.contains('isNative: true');
-        expect(logs[3].message).to.contains('isPureWeb: false');
-        expect(logs[4].message).to.contains('isRendererProcess: true');
-        expect(logs[5].message).to.contains('isMainProcess: false');
-        expect(logs[6].message).to.contains('isDarwin: ' + (process.platform === 'darwin').toString());
-        expect(logs[7].message).to.contains('isWin32: ' + (process.platform === 'win32').toString());
+        assert.equal(logs.length, 8);
+        assert.ok(logs[0].message.indexOf('isNode: true') !== -1);
+        assert.ok(logs[1].message.indexOf('isElectron: true') !== -1);
+        assert.ok(logs[2].message.indexOf('isNative: true') !== -1);
+        assert.ok(logs[3].message.indexOf('isPureWeb: false') !== -1);
+        assert.ok(logs[4].message.indexOf('isRendererProcess: true') !== -1);
+        assert.ok(logs[5].message.indexOf('isMainProcess: false') !== -1);
+        assert.ok(logs[6].message.indexOf('isDarwin: ' + (process.platform === 'darwin').toString()) !== -1);
+        assert.ok(logs[7].message.indexOf('isWin32: ' + (process.platform === 'win32').toString()) !== -1);
       });
   });
 });
 
-describe("electron without node-integration", function () {
+describe('electron without node-integration', function () {
   this.timeout(0);
   let app = null;
 
-  beforeEach(function () {
+  before(function () {
     app = new Application({
       path: electron,
       args: [path.join(__dirname, 'fixtures', 'app-no-node-integration')],
@@ -69,7 +94,7 @@ describe("electron without node-integration", function () {
     return app.start();
   });
 
-  afterEach(function () {
+  after(function () {
     if (app && app.isRunning()) {
       return app.stop();
     }
@@ -80,15 +105,15 @@ describe("electron without node-integration", function () {
       .waitUntilWindowLoaded()
       .getRenderProcessLogs()
       .then(function (logs) {
-        expect(logs.length).to.equal(8);
-        expect(logs[0].message).to.contains('isNode: false');
-        expect(logs[1].message).to.contains('isElectron: true');
-        expect(logs[2].message).to.contains('isNative: true');
-        expect(logs[3].message).to.contains('isPureWeb: false');
-        expect(logs[4].message).to.contains('isRendererProcess: true');
-        expect(logs[5].message).to.contains('isMainProcess: false');
-        expect(logs[6].message).to.contains('isDarwin: ' + (process.platform === 'darwin').toString());
-        expect(logs[7].message).to.contains('isWin32: ' + (process.platform === 'win32').toString());
+        assert.equal(logs.length, 8);
+        assert.ok(logs[0].message.indexOf('isNode: false') !== -1);
+        assert.ok(logs[1].message.indexOf('isElectron: true') !== -1);
+        assert.ok(logs[2].message.indexOf('isNative: true') !== -1);
+        assert.ok(logs[3].message.indexOf('isPureWeb: false') !== -1);
+        assert.ok(logs[4].message.indexOf('isRendererProcess: true') !== -1);
+        assert.ok(logs[5].message.indexOf('isMainProcess: false') !== -1);
+        assert.ok(logs[6].message.indexOf('isDarwin: ' + (process.platform === 'darwin').toString()) !== -1);
+        assert.ok(logs[7].message.indexOf('isWin32: ' + (process.platform === 'win32').toString()) !== -1);
       });
   });
 });
